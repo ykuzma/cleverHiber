c27b2c5851407e4574844b8cf101657414b7cac7
Yury Kuzma <vterkin1388@gmail.com>
2024-12-07 23:22:55 +0300

    test - add simple CarMapperTest

---
+++ src/test/java/by/kuzma/clever/hiber/mapper/CarMapperTest.java
@@ -0,0 +1,32 @@
+package by.kuzma.clever.hiber.mapper;
+
+import by.kuzma.clever.hiber.dto.CarDto;
+import by.kuzma.clever.hiber.dto.CategoryDto;
+import by.kuzma.clever.hiber.entity.Car;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarMapperTest {
+
+
+    CarMapper carMapper;
+
+    @Test
+    void toCar() {
+        //given
+        CategoryDto categoryDto = new CategoryDto(null, "pickup");
+        CarDto carDto = new CarDto(null,
+                "honda", "civic", 1990, 100, categoryDto);
+
+        //when
+        Car car = carMapper.toCar(carDto);
+        //then
+        assertThat(car).isNotNull()
+                .hasFieldOrPropertyWithValue("model", carDto.model())
+                .hasFieldOrPropertyWithValue("mark", carDto.mark());
+        assertThat(car.getCategory()).isNotNull()
+                .hasFieldOrPropertyWithValue("name", categoryDto.name());
+    }
+}
--- src/test/java/by/kuzma/clever/hiber/service/CarServiceImplTest.java
+++ src/test/java/by/kuzma/clever/hiber/service/CarServiceImplTest.java
@@ -1,15 +1,24 @@
 package by.kuzma.clever.hiber.service;
 
 import by.kuzma.clever.hiber.TestHelper;
+import by.kuzma.clever.hiber.dto.CarDto;
 import by.kuzma.clever.hiber.entity.Car;
+import by.kuzma.clever.hiber.entity.Category;
+import by.kuzma.clever.hiber.mapper.CarMapper;
+import by.kuzma.clever.hiber.mapper.CarMapperImpl;
 import by.kuzma.clever.hiber.repository.CarRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CarServiceImplTest {
@@ -19,6 +28,9 @@ class CarServiceImplTest {
     @Mock
     CarRepository carRepository;
 
+    @Spy
+    CarMapperImpl carMapper;
+
     @InjectMocks
     CarServiceImpl carService;
 
@@ -27,13 +39,24 @@ class CarServiceImplTest {
     }
 
     @Test
-    void shouldAddCar() {
+    void findById() {
         //given
-        Car car = new Car();
+        UUID id = UUID.randomUUID();
 
+        Car build = Car.builder()
+                .id(id)
+                .mark("honda")
+                .model("civic")
+                .price(100)
+                .yearOfProduction(1980)
+                .build();
+        CarDto expected = new CarDto(id, "civic", "honda", 1980, 100, null);
+
+        when(carRepository.findById(id)).thenReturn(build);
         //when
-        carService.addCar(car);
+        CarDto actual = carService.findById(id);
         //then
-        verify(carRepository).save(car);
+        assertThat(actual).isEqualTo(expected);
+
     }
 }
